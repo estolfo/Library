a69b5e0b495e3d781739542ac778809ae744a5a9
estolfo <emily@10gen.com>
2012-09-09 12:58:14 -0400

    changing db connection configs

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -1,7 +1,14 @@
 require 'mongo'
 
-connection = Mongo::Connection.new(URI.parse(ENV['DATABASE_URL'] || 'localhost'))
-DB         = connection.db('library')
+if ENV['RACK_ENV'] == 'production'
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  DB = Mongo::Connection.new(db.host, db.port).db(db_name)
+  DB.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+else
+  DB = Mongo::Connection.new("localhost", 27017).db('library')
+end
+
 USERS      = DB['users']
 BOOKS      = DB['books']
-AUTHORS    = DB['authors'] 
+AUTHORS    = DB['authors']
